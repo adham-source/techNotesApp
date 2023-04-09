c7a06b20ad6a79d989070ab7935a1649494c596f
adham-source <adham.ahmad1988as@gmail.com>
2023-04-09 08:45:52 +0200

    Completed project

--- client/src/app/api/apiSlice.js
+++ client/src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 import { setCredentials } from "../../features/auth/authSlice"
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://technotes-vkt9.onrender.com/api/v1",
+  baseUrl: "https://technotesapp-api.onrender.com/api/v1",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token
